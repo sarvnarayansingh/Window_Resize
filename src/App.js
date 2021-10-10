2165e2250b493c487fe60c809c3a01023e3f59a3
sarv narayan <sarvnarayan12@gmail.com>
2021-10-11 01:21:30 +0530

    funWindowResize

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 
 import './App.css';
 import ClassComptWindowResize from './components/ClassWindow'
+import useWindowSize from './components/FunCtionWIndowResize'
 
 function App() {
+  const windowSize = useWindowSize();
+  console.log("====hook",windowSize)
+
   return (
     <div className="App">
       <ClassComptWindowResize />
